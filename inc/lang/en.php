c522cac0b7ef900ad6705cb756a16da40897256f
Tautvydas Dulskis <tautvydas.dulskis@venipak.com>
2024-01-31 13:43:25 +0200

    Test

--- inc/lang/en.php
+++ inc/lang/en.php
@@ -18,32 +18,3 @@ $LANG['menu']['manifest_add'] = 'New shipment list';
 $LANG['menu']['manifest'] = 'View the shipment lists';
 $LANG['menu']['import'] = 'Import shipment data';
 
-$LANG['menu']['tracking'] = 'Shipment status/P.O.D.';
-$LANG['menu']['report'] = 'Reports';
-$LANG['menu']['siunt_report'] = 'Basic report';
-$LANG['menu']['siunt_report_cod'] = 'COD report';
-$LANG['menu']['siunt_report_returns'] = 'Returns report';
-$LANG['menu']['siunt_report_con_returns'] = 'Consolidated returns report';
-
-$LANG['menu']['client'] = 'Clients';
-$LANG['menu']['kl_new'] = 'New client';
-$LANG['menu']['kl_view'] = 'Find client';
-$LANG['menu']['kl_import'] = 'Import client data';
-$LANG['menu']['kl_manage'] = 'Manage client data';
-
-$LANG['menu']['help'] = 'Help';
-$LANG['menu']['contact'] = 'Contacts';
-$LANG['menu']['logoff'] = 'Log off';
-
-$LANG['cont']['support_14_phone'] = '+370 700 55221';
-$LANG['cont']['support_14_phone2'] = '+370 5 210 7717';
-$LANG['cont']['support_14_kas'] = 'helpdesk.lt@venipak.com';
-$LANG['cont']['support_14_kont'] = 'uzsakymai@venipak.lt';
-$LANG['cont']['support_156432_phone'] = '+371 6601 6601';
-$LANG['cont']['support_156432_phone2'] = '+371 6601 6601';
-$LANG['cont']['support_156432_kas'] = 'helpdesk.lv@venipak.com';
-$LANG['cont']['support_156432_kont'] = 'sales@venipak.lv';
-$LANG['cont']['support_157407_phone'] = '+372 641 4481';
-$LANG['cont']['support_157407_phone2'] = '+372 5860 2261';
-$LANG['cont']['support_157407_kas'] = 'helpdesk.ee@venipak.com';
-$LANG['cont']['support_157407_kont'] = 'helpdesk.ee@venipak.com';
